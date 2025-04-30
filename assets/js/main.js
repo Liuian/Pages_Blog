11e4695a808f8a2dc56e56e72e18f2532b73f49f
ianliu <ianliu@ghtinc.com>
2025-04-30 12:43:37 +0800

    add navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,14 +28,14 @@ document.addEventListener("DOMContentLoaded", function () {
         window.scrollTo({ top: 0, behavior: "smooth" });
     });
 
-    // Back to home 功能（只有在不是首頁時顯示）
-    if (window.location.pathname !== "/pages-blog/" && window.location.pathname !== "/pages-blog/index.html") {
-        const backHomeLink = document.createElement("a");
-        backHomeLink.textContent = "← 🏠︎ ";
-        backHomeLink.href = "https://liuian.github.io/pages-blog/";
-        backHomeLink.className = "back-home-link";
-        document.body.appendChild(backHomeLink);
-    }
+    // // Back to home 功能（只有在不是首頁時顯示）
+    // if (window.location.pathname !== "/pages-blog/" && window.location.pathname !== "/pages-blog/index.html") {
+    //     const backHomeLink = document.createElement("a");
+    //     backHomeLink.textContent = "← 🏠︎ ";
+    //     backHomeLink.href = "https://liuian.github.io/pages-blog/";
+    //     backHomeLink.className = "back-home-link";
+    //     document.body.appendChild(backHomeLink);
+    // }
 
     // 捲動到一定高度後顯示「Top」按鈕
     window.addEventListener("scroll", function () {
